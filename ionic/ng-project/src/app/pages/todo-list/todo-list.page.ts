efd75317df33e268c3b648aef288ff70dc3a8845
Shohra Qadri <shohra.qadri@paavu.com>
2023-10-16 19:05:22 +0530

    page ready for QA1

--- ionic/ng-project/src/app/pages/todo-list/todo-list.page.ts
+++ ionic/ng-project/src/app/pages/todo-list/todo-list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,18 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListPage implements OnInit {
 
-  constructor() { }
+  constructor(private toastController: ToastController) { }
 
   ngOnInit() {
   }
   inputValue: string = '';
   list: any[] = [];
-  myfun() {
+  // isEdit: any = false;
+  inputName: any;
+  async myfun() {
+    if (this.inputValue == '' || this.inputValue == undefined) {
+      const toast = await this.toastController.create({
+        message: 'please fill the input field!',
+        duration: 1500,
+        position: 'bottom',
+      });
 
+      await toast.present();
+      return;
+    }
     this.list.push({ id: this.list.length, name: this.inputValue });
     console.log(this.list)
   }
   removebutton(id: number) {
-    alert('hii')
+    console.warn(id)
+    this.list = this.list.filter(inputValue => inputValue.id !== id)
+  }
+  editbutton(value: any, name: any) {
+    console.log(this.list);
+    // this.isEdit = true;
+    // const found = list.find((value) => element > 10);
+    // this.inputName = name;
+    // console.log("inputValue === ", value);
+    // console.warn('hii')
   }
 }
