1e1a096f6f331f15cb68d3d22475238162f4481b
Shohra Qadri <shohra.qadri@paavu.com>
2023-12-18 18:57:17 +0530

    page working

--- javascript/arrow.js
+++ javascript/arrow.js
@@ -1,11 +1,23 @@
-const user = {
-  username: "shohra",
-  price: 888,
-  welcomemessage: function () {
-    console.log(`${this.username} , welocme in your account`);
-    console.log(this)
-  } 
+// const user = {
+//   username: "shohra",
+//   price: 888,
+//   welcomemessage: function () {
+//     console.log(`${this.username} , welocme in your account`);
+//     console.log(this)
+//   }
+// }
+// user.welcomemessage()
+// user.username = "Qadri"
+// user.welcomemessage()
+// console.log(this);
+
+
+(function chai() {
+  console.log(`databae connected.`);
 }
-user.welcomemessage()
-user.username ="Qadri"
-user.welcomemessage()
+)();
+
+((name) => {
+  console.log(`another database connented ${name}`);
+}
+)("Qadri")
